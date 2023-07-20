c83acde6a23298b40af6f8958c4acef2f2f4f3b9
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-07-20 21:41:30 +0530

    JS file included

---
+++ script.js
@@ -0,0 +1,37 @@
+let movies =[
+    {
+        name:"loki",
+        des:
+        "Follow the adventures of Loki, the God of Mischief, after the events of ",
+        image:"images/slider 1.png"
+    },
+    {
+        name:"falcon and the winter soldier",
+        des:
+        "Set in the aftermath of this action-packed series follows Sam Wilson (Falcon) and Bucky Barnes (The Winter Soldier) as they team up for a global adventure.",
+
+        image:"images/slider 2.png"
+    },
+    {
+        name:"wanda vision",
+        des:
+        "tep into a surreal world where Wanda Maximoff (Scarlet Witch) and Vision live idyllic suburban lives. ",
+
+        image:"images/slider 3.png"
+    },
+    {
+        name:"raya and the last dragon",
+        des:
+        "mbark on a magical quest with Raya, a brave warrior, as she sets out to find the last dragon and restore peace to her fractured land. ",
+
+        image:"images/slider 4.png"
+    },
+    {
+        name:"luca",
+        des:
+        "Dive into the enchanting coastal town of Portorosso in Italy, where you'll meet Luca, a young sea monster who dreams of experiencing life on land.",
+
+        image:"images/slider 5.png"
+    },
+ 
+]
