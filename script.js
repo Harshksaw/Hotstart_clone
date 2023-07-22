634b8c74bce55732a3f9ee64101b4399dde0b3c0
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-07-22 14:44:24 +0530

    Added video cards

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 let movies = [
   {
-    name: "loki",
-    des: "Follow the adventures of Loki, the God of Mischief, after the events of ",
-    image: "images/slider 1.png",
+      name: 'Loki',
+      des: 'Loki, the God of Mischief, steps out of his brothers shadow to embark on an adventure that takes place after the events of Avengers: Endgame',
+      image:'images/slider 1.PNG'
   },
   {
-    name: "falcon and the winter soldier",
-    des: "Set in the aftermath of this action-packed series follows Sam Wilson (Falcon) and Bucky Barnes (The Winter Soldier) as they team up for a global adventure.",
-
-    image: "images/slider 2.png",
+      name: 'Falcon and the winter soldier',
+      des: 'Falcon and the Winter Soldier are a mismatched duo who team up for a global adventure that will test their survival skills -- as well as their patience.',
+      image:'images/slider 2.PNG'
   },
   {
-    name: "wanda vision",
-    des: "tep into a surreal world where Wanda Maximoff (Scarlet Witch) and Vision live idyllic suburban lives. ",
-
-    image: "images/slider 3.png",
+      name: "Wanda Vision",
+      des: "Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems.",
+      image:"images/slider 3.PNG"
   },
   {
-    name: "raya and the last dragon",
-    des: "mbark on a magical quest with Raya, a brave warrior, as she sets out to find the last dragon and restore peace to her fractured land. ",
-
-    image: "images/slider 4.png",
+      name: 'Raya and the last dragon',
+      des: 'Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people.',
+      image: 'images/slider 4.PNG'
   },
   {
-    name: "luca",
-    des: "Dive into the enchanting coastal town of Portorosso in Italy, where you'll meet Luca, a young sea monster who dreams of experiencing life on land.",
-
-    image: "images/slider 5.png",
-  },
-];
-const carousel = document.querySelector(".carousel");
+      name: 'Luca',
+      des: 'Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides. Luca shares these adventures with his newfound best friend, but all the fun is threatened by a deeply held secret: he is a sea monster from another world just below the water surface.',
+      image: 'images/slider 5.PNG'
+  }
+]
+const carousel = document.querySelector('.carousel');
 let sliders = [];
 
 let slideIndex = 0; //track the current slide
@@ -44,24 +40,37 @@ const createSlide = () => {
   let content = document.createElement('div');
   let h1 = document.createElement('h1');
   let p = document.createElement('p');
+  
   //attaching all the elements
-  imgElement.appendChild(document.createTextNode(''));
+
+
+  carousel.appendChild(slide); //   <!-- <div class="slider">
+ 
   h1.appendChild(document.createTextNode(movies[slideIndex].name));
   p.appendChild(document.createTextNode(movies[slideIndex].des));
-  content.appendChild(h1);
-  content.appendChild(p);
-  slide.appendChild(imgElement);
-  carousel.appendChild(slide);
+  slide.appendChild(content); //  <!-- <div class="slider"> 
+  //content ->>
+  content.appendChild(h1); 
+  content.appendChild(p); 
+  // <div class="slide-content">
+  // <h1 class="movie-title">loki</h1>
+  // <p class="movie-des"> Lorem ipsu em</p></div>
+  
+  slide.appendChild(imgElement); // <img  class="slider-img" src="images/slider 1.PNG" alt ="" />
+  imgElement.appendChild(document.createTextNode(''));
+  
   //settings up images
 
   imgElement.src = movies[slideIndex].image;
+  //src="images/slider i .PNG
   slideIndex++;
 
   //settings elements classnames
-  slide.className = "slider";
-  content.className = "slide-content";
-  h1.className = "movie-title";
-  p.className = "movie-des";
+  slide.className = 'slider';
+  content.className = 'slide-content';
+  h1.className = 'movie-title';
+  p.className = 'movie-des';
+  imgElement.className = 'slider-img'
 
   sliders.push(slide);
   if (sliders.length) {
@@ -97,3 +106,20 @@ for(let i = 0; i < 3; i++) {
 setInterval(() => {
   createSlide();
 }, 3000);
+
+
+//video cards
+
+const videoCards = [...document.querySelectorAll('.video-card')];
+videoCards.forEach(item =>{
+  item.addEventListener('mouseover', () =>{
+    let video = item.children[1];
+    video.play()
+  })
+  item.addEventListener('mouseleave', () =>{
+    let video = item.children[1];
+    video.pause()
+  });
+})
+
+//card sliders
